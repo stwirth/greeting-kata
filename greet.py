bc52f566c4235efe440f84abdeb80bf38def1f1f
Lucas Chiesa <lucas.chiesa@gmail.com>
2018-05-24 10:52:58 +0200

    GREEN: Req 5

--- greet.py
+++ greet.py
@@ -1,13 +1,17 @@
 
 
-def greet(name):
-    if name is None:
-        name = 'my friend'
+def greet(names):
+    if names is None:
+        names = 'my friend'
 
-    if type(name) == list:
-        return 'Hello, ' + ' and '.join(name) + '.'
+    if type(names) == list and len(names) == 2:
+        return 'Hello, ' + ' and '.join(names) + '.'
 
-    if name.upper() == name:
-        return 'HELLO {}!'.format(name)
+    if type(names) == list and len(names) > 2:
+        comma_separated = ', '.join(names[:-1])
+        return 'Hello, {}, and {}.'.format(comma_separated, names[-1])
 
-    return 'Hello, {}.'.format(name)
+    if names.upper() == names:
+        return 'HELLO {}!'.format(names)
+
+    return 'Hello, {}.'.format(names)
