85d3d1fe748cef157efa5f7108107a479eda1c4d
Stephan Wirth <1481786+stwirth@users.noreply.github.com>
2018-05-24 11:06:23 +0200

    GREEN: Req 6

--- greet.py
+++ greet.py
@@ -2,16 +2,29 @@
 
 def greet(names):
     if names is None:
-        names = 'my friend'
+        names = ['my friend']
 
-    if type(names) == list and len(names) == 2:
-        return 'Hello, ' + ' and '.join(names) + '.'
+    if type(names) != list:
+        names = [names]
 
-    if type(names) == list and len(names) > 2:
-        comma_separated = ', '.join(names[:-1])
-        return 'Hello, {}, and {}.'.format(comma_separated, names[-1])
+    normal_list = [name for name in names if not name.isupper()]
+    shout_list = [name for name in names if name.isupper()]
 
-    if names.upper() == names:
-        return 'HELLO {}!'.format(names)
+    normal_greeting = ''
+    shout_greeting = ''
 
-    return 'Hello, {}.'.format(names)
+    if len(normal_list) == 2:
+        normal_greeting = 'Hello, ' + ' and '.join(normal_list) + '.'
+    elif len(normal_list) > 2:
+        comma_separated = ', '.join(normal_list[:-1])
+        normal_greeting  = 'Hello, {}, and {}.'.format(comma_separated, normal_list[-1])
+    elif len(normal_list) == 1:
+        normal_greeting = 'Hello, {}.'.format(normal_list[0])
+
+    if len(shout_list) > 0:
+        shout_greeting = 'HELLO {}!'.format(' '.join(shout_list))
+
+    if len(normal_greeting) > 0 and len(shout_greeting):
+        shout_greeting = ' AND ' + shout_greeting
+
+    return normal_greeting + shout_greeting
