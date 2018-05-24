8b98c8ca60d03709771bef887d3216d313b2a265
Lucas Chiesa <lucas.chiesa@gmail.com>
2018-05-24 10:37:43 +0200

    RED: Fail null test

--- test_greet.py
+++ test_greet.py
@@ -6,3 +6,6 @@ class GreetTest(unittest.TestCase):
 
     def test_greet(self):
         self.assertEqual(greet('Bob'), 'Hello, Bob.')
+
+    def test_null_name(self):
+        self.assertEqual(greet(None), 'Hello, my friend.')
