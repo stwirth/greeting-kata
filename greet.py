be57461abe036c333bdc8d30a8d0ca00aff088c2
Stephan Wirth <1481786+stwirth@users.noreply.github.com>
2018-05-24 10:45:52 +0200

    GREEN: Req 4

--- greet.py
+++ greet.py
@@ -4,6 +4,9 @@ def greet(name):
     if name is None:
         name = 'my friend'
 
+    if type(name) == list:
+        return 'Hello, ' + ' and '.join(name) + '.'
+
     if name.upper() == name:
         return 'HELLO {}!'.format(name)
 
